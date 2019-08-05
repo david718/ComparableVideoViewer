4e58cd986b975fe49c7fc46d04fae467f6b28603
Hwang Kyeong Chan <hkc7180@gmail.com>
2019-08-06 08:49:00 +0900

    start to drag and drop bar element

--- src/components/ComparableVideoViewer.tsx
+++ src/components/ComparableVideoViewer.tsx
@@ -11,43 +11,45 @@ const ComparableVideoViewer: React.SFC<Props> = ({ src }) => {
   const canvasRef: React.RefObject<HTMLCanvasElement> = React.useRef(null);
   const highCanvasRef: React.RefObject<HTMLCanvasElement> = React.useRef(null);
 
-  const interval = setInterval(() => {
-    if (video) {
-      const videoToCanvas = (
-        video: any,
-        canvas: any,
-        position: 'left' | 'right',
-        width: number,
-        bar: boolean
-      ) => {
-        if (canvas) {
-          canvas.width = width;
-          canvas.height = video.videoHeight;
+  const videoToCanvasOptionalBar = (
+    video: any,
+    canvas: any,
+    position: 'left' | 'right',
+    width: number,
+    bar: boolean
+  ) => {
+    if (canvas) {
+      canvas.width = width;
+      canvas.height = video.videoHeight;
 
-          const ctx = canvas.getContext('2d');
-
-          ctx.drawImage(
-            video,
-            position === 'left' ? 0 : video.videoWidth / 2,
-            0,
-            width,
-            video.videoHeight,
-            0,
-            0,
-            width,
-            video.videoHeight
-          );
-          if (bar) {
-            ctx.rect(width - 10, 0, 10, video.videoHeight);
-            ctx.fill();
-          }
-        }
-      };
-
-      videoToCanvas(video, highCanvasRef.current, 'right', video.videoWidth / 2, false);
-      videoToCanvas(video, canvasRef.current, 'left', video.videoWidth / 4, true);
+      const ctx = canvas.getContext('2d');
+      ctx.beginPath();
+      ctx.drawImage(
+        video,
+        position === 'left' ? 0 : video.videoWidth / 2,
+        0,
+        width,
+        video.videoHeight,
+        0,
+        0,
+        width,
+        video.videoHeight
+      );
+      if (bar) {
+        ctx.beginPath();
+        ctx.rect(width - 10, 0, 10, video.videoHeight);
+        ctx.fill();
+      }
     }
-  });
+  };
+  const twoVideosToCanvas = () => {
+    if (video) {
+      videoToCanvasOptionalBar(video, highCanvasRef.current, 'right', video.videoWidth / 2, false);
+      videoToCanvasOptionalBar(video, canvasRef.current, 'left', video.videoWidth / 4, true);
+    }
+  };
+
+  const interval = setInterval(twoVideosToCanvas);
   React.useEffect(() => {
     return () => clearInterval(interval);
   }, []);
@@ -67,7 +69,7 @@ const ComparableVideoViewer: React.SFC<Props> = ({ src }) => {
   return (
     <div style={{ position: 'relative' }}>
       <canvas style={{ position: 'absolute', top: 0, left: 0 }} ref={highCanvasRef} />
-      <canvas style={{ position: 'absolute', top: 0, left: 0 }} ref={canvasRef}/>
+      <canvas style={{ position: 'absolute', top: 0, left: 0 }} ref={canvasRef} />
       <video style={{ display: 'none' }} ref={videoRef} src={src} controls={true} />
       <div
         style={{ position: 'absolute', top: video ? video.videoHeight : 360, left: 0, padding: 10 }}
